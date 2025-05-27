316bb7b0d6af66b61b33c7a653f7e0f3626db472
kuwatsuru <bb.kosuke@gmail.com>
2025-05-27 09:58:25 +0900

    standalone

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-require('dotenv').config()
+require("dotenv").config();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        // Reference a variable that was defined in the .env file and make it available at Build Time
-        API_ENDPOINT: process.env.API_ENDPOINT,
-      },
-}
+  distDir: "build",
+  output: "standalone",
+};
 
-module.exports = nextConfig
+// const nextConfig = {
+//     env: {
+//         // Reference a variable that was defined in the .env file and make it available at Build Time
+//         API_ENDPOINT: process.env.API_ENDPOINT,
+//       },
+// }
+
+module.exports = nextConfig;
